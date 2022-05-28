79f1348e11c4df0541113e946553c7bab633c960
Israt Munia <isratmunia000@gmail.com>
2022-05-28 19:20:21 +0600

    blogs done

--- safety-tools/src/pages/Blogs/Blogs.js
+++ safety-tools/src/pages/Blogs/Blogs.js
@@ -1,5 +1,5 @@
 import React from "react";
-import './Blogs.css';
+import "./Blogs.css";
 
 const Blogs = () => {
   return (
@@ -8,51 +8,84 @@ const Blogs = () => {
 
       <div className="blogs-container">
         <div className="blog">
-          <h3>Difference between javascript and nodejs.</h3>
+          <h3>How will you improve the performance of a React Application?</h3>
+          <br />
+          <h5>
+            <strong>
+              Performance of a React Application can be improved by-
+            </strong>
+          </h5>
           <p>
-            JavaScript is normally used for any client-side activity for one web
-            application. Whereas Node JS is mainly used for accessing or running
-            any operating system for non-blocking operation. JavaScript is a
-            simple programming language that runs in any browser JavaScript
-            Engine. Whereas Node JS is an interpreter or running environment for
-            a JavaScript programming language that holds many excesses, it
-            requires libraries that can easily be accessed from JavaScript
-            programming for better use.
+            Keeping component state local where necessary. <br />
+            Memoizing React components to prevent unnecessary re-renders. <br />
+            Code-splitting in React using dynamic import <br />
+            Windowing or list virtualization in React. <br />
+            Lazy loading images in React.
           </p>
         </div>
         <div className="blog">
-          <h3>When should you use nodejs and when should you use mongodb?</h3>
+          <h3>
+            What are the different ways to manage a state in a React
+            application?
+          </h3>
+          <br />
+          <h5>
+            <strong>We can manage a state in a React application with-</strong>
+          </h5>
           <p>
-            NodeJS is a JavaScript runtime environment. It actually helps
-            JavaScript to run outside of the server. It's used in server-side
-            development. And if we need a database to store the data or
-            information so here we can use MongoDB. But to be connected with
-            MongoDB we need a connector, so here we can use NodeJS which will
-            help our website to run outside of the server.
+            <li>Useing useReducer for Complex State</li>
+            <li>Using Custom Hooks</li>
+            <li>Using Context API</li>
+            <li>Using Data Fetching Libraries</li>
+            <li>Using localStorage and URL params</li>
           </p>
         </div>
         <div className="blog">
-          <h3>Differences between SQL and NoSQL databases.</h3>
+          <h3>How does prototypical inheritance work?</h3>
           <p>
-            SQL databases display data in form of tables so it is known as
-            table-based database. NoSQL databases display data as collection of
-            key-value pairs, documents, graph databases, or wide-column stores.
-            SQL databases have fixed or static or predefined schema. NoSQL
-            databases have dynamic schema. SQL databases use a powerful language
-            "Structured Query Language" to define and manipulate the data. In
-            NoSQL databases, collection of documents are used to query the data.
+            prototypical inheritance refers to the ability to access object
+            properties from another object. We use a JavaScript prototype to add
+            new properties and methods to an existing object constructor. We can
+            then essentially tell our JS code to inherit properties from a
+            prototype. Prototypical inheritance allows us to reuse the
+            properties or methods from one JavaScript object to another through
+            a reference pointer function.
           </p>
         </div>
         <div className="blog">
-          <h3>What is the purpose of JWT and how does it work?</h3>
+          <h3>
+            Why you do not set the state directly in React. For example, if you
+            have const [products, setProducts] = useState([]). Why you do not
+            set products = [...] instead, you use the setProducts
+          </h3>
           <p>
-            JWT or JSON Web Token is an open standard for securely transmitting
-            information between parties as JSON object. The purpose of using JWT
-            is to ensure the authenticity of the data. JWT is signed and
-            encoded, not encrypted. JWT is a token-based stateless
-            authentication mechanism. Since it is a client-side-based stateless
-            session, the server doesn’t have to completely rely on a database to
-            save session information.
+            setState() enqueues changes to the component state and tells React
+            that this component and its children need to be re-rendered with the
+            updated state. This is the primary method use to update the user
+            interface in response to event handlers and server responses.
+          </p>
+        </div>
+        <div className="blog">
+          <h3>
+            You have an array of products. Each product has a name, price,
+            description, etc. How will you implement a search to find products
+            by name?
+          </h3>
+          <p>const result = products.find(product = product.name === "name");</p>
+        </div>
+        <div className="blog">
+          <h3>What is a unit test? Why should write unit tests?</h3>
+          <p>
+            Unit tests are typically automated tests written and run by software
+            developers to ensure that a section of an application meets its
+            design and behaves as intended. In procedural programming, a unit
+            could be an entire module, but it is more commonly an individual
+            function or procedure. <br />
+            One of the benefits of unit tests is that they isolate a function,
+            class or method and only test that piece of code. Higher quality
+            individual components create overall system resiliency. Thus, the
+            result is reliable code. Unit tests also change the nature of the
+            debugging process.
           </p>
         </div>
       </div>
